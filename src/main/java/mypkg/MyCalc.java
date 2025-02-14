725be9d57f3980ceb2f5043e8c5eba5886b7cf86
vishnuminnu <vishnuvardhan123987@gmail.com>
2025-02-14 09:38:50 +0530

    "second commit"

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -7,9 +7,13 @@ public class MyCalc {
 	public int diff(int a,int b) {
 		return a-b;
 	}
+	public int mul(int a,int b) {
+		return a*b;
+	}
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
 		System.out.println(" Sum is : "+calc.sum(20, 10));
+		System.out.println(" mul is : "+calc.mul(20, 10));
 		System.out.println(" diff is : "+calc.diff(20, 10));
 	}
 
