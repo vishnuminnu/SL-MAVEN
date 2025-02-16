221118cbc89297865cede3dd2787f19129bb87d9
vishnuminnu <vishnuvardhan123987@gmail.com>
2025-02-16 10:20:25 +0530

    show() & greeting() added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -10,11 +10,19 @@ public class MyCalc {
 	public int mul(int a,int b) {
 		return a*b;
 	}
+	public void show() {
+		System.out.println(" Hello Jenkins")
+	}
+	public void greeting() {
+		System.out.println(" Hello Jenkins")
+	}
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
 		System.out.println(" Sum is : "+calc.sum(20, 10));
 		System.out.println(" mul is : "+calc.mul(20, 10));
 		System.out.println(" diff is : "+calc.diff(20, 10));
+		calc.show();
+		calc.greeting();
 	}
 
 }
